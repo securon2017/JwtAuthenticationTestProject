d32d4573e831b50d06c12f06ede5dce44726f9e8
Dmitriy <securon2003@gmail.com>
2023-01-14 21:10:03 +0200

    Added LoginModel

---
+++ Auth/LoginModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JwtAuthenticationTestProject.Auth
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "User Name is requared")]
+        public string? UserName { get; set; }
+
+        [Required(ErrorMessage = "Password is requared")]
+        public string? Password { get; set; }
+
+    }
+}
