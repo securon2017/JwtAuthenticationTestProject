516c1a0702938e5a05261b0f520b48ec6104ab60
Dmitriy <securon2003@gmail.com>
2023-01-14 21:05:57 +0200

    Added RegisterModel

---
+++ Auth/RegisterModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JwtAuthenticationTestProject.Auth
+{
+    public class RegisterModel
+    {
+        [Required (ErrorMessage ="User Name is requared")]
+        public string? UserName { get; set; }
+
+        [EmailAddress]
+        [Required(ErrorMessage = "Email is requared")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Password is requared")]
+        public string? Password { get; set; }
+    }
+}
